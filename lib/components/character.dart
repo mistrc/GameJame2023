7ef9315e24d1a41c4011af1356984310eed620cd
Chirag Mistry <chiragwork@poppyfields.biz>
2023-12-09 12:44:50 +0000

    little bit of privatification given things seem to be working

--- lib/components/character.dart
+++ lib/components/character.dart
@@ -10,16 +10,19 @@ import '../utilities/constants.dart';
 enum DirectionOfMovement { left, right }
 
 class Character extends PositionComponent {
-  final Sprite person;
-  final Vector2 centerOfRotation;
-  final double radiusToEdge;
-  static const bufferFromEdge = 10.0;
+  final Sprite _person;
+  final double _radiusToEdge;
+  static const _bufferFromEdge = 10.0;
 
   /// A measure of how fast the character is moving round the tunnel
-  double angularMomentum = 0;
+  double _angularMomentum = 0;
 
-  Character({required this.centerOfRotation, required this.radiusToEdge})
-      : person = Sprite(
+  /// Multiplier to apply to how much gravity caused the character to go back to the bottom of the tunnel
+  static const _gravitationalConstant = 0.1;
+
+  Character({required Vector2 centerOfRotation, required double radiusToEdge})
+      : _radiusToEdge = radiusToEdge,
+        _person = Sprite(
           Flame.images.fromCache(spriteFileName),
           srcPosition: Vector2(1176, 17),
           srcSize: Vector2(172, 183),
@@ -31,10 +34,10 @@ class Character extends PositionComponent {
 
   @override
   void render(Canvas canvas) {
-    person.render(canvas,
-        position: Vector2(0, radiusToEdge - bufferFromEdge),
+    _person.render(canvas,
+        position: Vector2(0, _radiusToEdge - _bufferFromEdge),
         anchor: Anchor.bottomCenter,
-        size: person.srcSize.scaled(0.3));
+        size: _person.srcSize.scaled(0.3));
 
     super.render(canvas);
   }
@@ -42,26 +45,24 @@ class Character extends PositionComponent {
   void move(DirectionOfMovement movement) {
     switch (movement) {
       case DirectionOfMovement.left:
-        angularMomentum += pi / 16;
+        _angularMomentum += pi / 16;
         break;
 
       case DirectionOfMovement.right:
-        angularMomentum -= pi / 16;
+        _angularMomentum -= pi / 16;
         break;
     }
   }
 
-  static const gravitationalConstant = 0.1;
-
   @override
   void update(double dt) {
     final angleInTunnel = angle % (2 * pi);
-    angularMomentum -= gravitationalConstant * sin(angleInTunnel);
+    _angularMomentum -= _gravitationalConstant * sin(angleInTunnel);
 
     /// Don't want the character osculating forever, so applying some damping
-    angularMomentum *= 0.995;
+    _angularMomentum *= 0.995;
 
-    angle += angularMomentum * dt;
+    angle += _angularMomentum * dt;
     super.update(dt);
   }
 }
